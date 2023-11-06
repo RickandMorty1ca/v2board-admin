112d8584bb2c038c680ba438b227d98a8f83cc8b
Sam <9622944+sam-sun00@users.noreply.github.com>
2023-11-06 15:13:10 -0800

    Update and rename env.example.js to env.js

---
+++ env.js
@@ -0,0 +1,18 @@
+window.settings = {
+  // 站点标题
+  title: '瑞克和莫蒂的时光机',
+  // API
+  host: 'https://rickandmorty1.ca',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'black'
+  },
+  // 背景
+  // old background_url: 'https://images3.alphacoders.com/253/253399.png',
+  background_url: 'https://img10.360buyimg.com/babel/jfs/t1/209612/35/23660/213305/62b2f856E2ec4c037/5fd2c52abc19e0e4.png',
+  logo: '',
+  // 需与V2Board设置中的后台路径一致
+  secure_path: 'admin'
+}
